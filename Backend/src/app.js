1772e2081fe4907a8fbb765a85c2788119a4163c
NamanVaish <namanvaish53@gmail.com>
2026-07-27 00:49:10 +0530

    setting up file,routes,prefix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const authRouter = require("../src/routes/auth.routes.js");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
 
 module.exports = app;
