c2faf9567fcefd2803470501fe13e014c9ae49db
NamanVaish <namanvaish53@gmail.com>
2026-08-02 17:13:32 +0530

    Update auth.controller.js

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -38,7 +38,12 @@ const registerUserController = async (req, res) => {
         { expiresIn: "1d" }
     );
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 24 * 60 * 60 * 1000,
+});
 
     res.status(201).json({
         message: "User registered successfully",
@@ -78,7 +83,13 @@ const loginUserController = async (req, res) => {
         { expiresIn: "1d" }
     );
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 24 * 60 * 60 * 1000,
+});
+    
     res.status(200).json({
         message: "User loggedIn successfully",
         user: {
