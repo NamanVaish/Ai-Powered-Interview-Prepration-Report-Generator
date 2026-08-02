3db6ef4fbfb6f98ecd857feeb85df475ca174d58
NamanVaish <namanvaish53@gmail.com>
2026-08-02 18:29:37 +0530

    Update auth.controller.js

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -46,6 +46,7 @@ const registerUserController = async (req, res) => {
     maxAge: 24 * 60 * 60 * 1000,
 });
     console.log(res.getHeaders()["set-cookie"]);
+    console.log(res.getHeaders());
     res.status(201).json({
         message: "User registered successfully",
         user: {
@@ -92,6 +93,7 @@ const loginUserController = async (req, res) => {
     maxAge: 24 * 60 * 60 * 1000,
 });
     console.log(res.getHeaders()["set-cookie"]);
+    console.log(res.getHeaders());
     console.log("NODE_ENV:", process.env.NODE_ENV);
     
     res.status(200).json({
