812c349763eb7b0c36445f3cfa0cb16f6fa32543
NamanVaish <namanvaish53@gmail.com>
2026-08-03 12:19:40 +0530

    Update auth.middleware.js

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -26,11 +26,6 @@ const authUser = async (req, res, next) => {
             message: "Invalid token"
         });
     }
-
-    console.log("Origin:", req.headers.origin);
-console.log("Cookie Header:", req.headers.cookie);
-console.log("Parsed Cookies:", req.cookies);
-console.log("Token:", req.cookies.token);
 };
 
 module.exports = { authUser };
