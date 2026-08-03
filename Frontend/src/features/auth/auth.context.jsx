5b1c7799601793ba925c38be14d89c4c7be6112a
NamanVaish <namanvaish53@gmail.com>
2026-08-03 11:56:06 +0530

    Update auth.context.jsx

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,10 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const getAndSetUser = async () => {
+            try {
+                const data = await getMe();
+
+                if (data?.user) {
+                    setUser(data.user);
+                } else {
+                    setUser(null);
+                }
+            } catch (err) {
+                console.error(err);
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        getAndSetUser();
+    }, []);
 
     return (
         <AuthContext.Provider value={{ user, setUser, loading, setLoading }}>
